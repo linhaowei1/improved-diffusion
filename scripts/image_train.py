31a8323f27d5242f724fc38943257d8b5ed4a1aa
haowei <linhaowei@pku.edu.cn>
2024-03-06 12:17:32 +0800

    fast push

--- scripts/image_train.py
+++ scripts/image_train.py
@@ -63,7 +63,7 @@ def create_argparser():
         schedule_sampler="uniform",
         lr=1e-4,
         weight_decay=0.0,
-        lr_anneal_steps=8000,
+        lr_anneal_steps=60000,
         batch_size=1,
         microbatch=-1,  # -1 disables microbatches
         ema_rate="0.9999",  # comma-separated list of EMA values
